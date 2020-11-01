825513dd2a190006b761f18d1df04c9048a574ca
ynportfolios <y.n.portfolios@gmail.com>
2020-11-01 17:27:46 +0900

    簡単ログイン実装（４）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   def check_guest
     email = resource&.email || params[:user][:email].downcase
-    if email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーの編集・削除はできません。'
-    end
+    redirect_to root_path, alert: 'ゲストユーザーの編集・削除はできません。' if email == 'guest@example.com'
   end
 end
