372535f2ae64b65add99f4c6507076e2a9884413
ynportfolios <y.n.portfolios@gmail.com>
2020-12-05 11:37:47 +0900

    性別ごとの登録者数表示機能の実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,25 @@
 class HomesController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+    @man_count = 0
+    @woman_count = 0
+    @other_count = 0
+    @no_answer_count = 0
+    @users.each do |user|
+      case user.sex
+      when 'man' then
+        @man_count += 1
+      when 'woman' then
+        @woman_count += 1
+      when 'other' then
+        @other_count += 1
+      when 'no_answer' then
+        @no_answer_count += 1
+      else
+        @no_answer_count += 1
+      end
+    end
+  end
 
   def show
     @q = User.ransack(params[:q])
