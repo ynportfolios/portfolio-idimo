a6ceb015052d529bf790170904e8b11f97c51de9
ynportfolios <y.n.portfolios@gmail.com>
2020-12-26 23:21:29 +0900

    テストコードを実装（２）

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'http://test.host' }
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
       enable_starttls_auto: true,
       address: "smtp.gmail.com",
--- spec/requests/homes_request_spec.rb
+++ spec/requests/homes_request_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe 'indexページ' do
-    it '表示に成功する' do
+  describe 'ログインせずにindexページを取得する' do
+    it '取得に成功し、正しいリンクのみ存在する' do
       get root_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include '<a class="navbar-brand" href="/">idimo</a>'
+      expect(response.body).to include '<a class="nav-link" href="/users/sign_up">新規登録</a>'
+      expect(response.body).to include '<a class="nav-link" href="/users/sign_in">ログイン</a>'
+      expect(response.body).to include '<a class="nav-link" rel="nofollow" data-method="post" href="/users/guest_sign_in">ゲストログイン（閲覧用）</a>'
+      expect(response.body).to_not include '<a class="nav-link" href="/homes/show">ユーザ検索</a>'
+      expect(response.body).to_not include '<a class="nav-link" href="/users/show?id='
+      expect(response.body).to_not include '">マイプロフィール</a>'
+      expect(response.body).to_not include '<a class="nav-link" href="/users/edit">アカウント編集</a>'
+      expect(response.body).to_not include '<a class="nav-link" href="/homes/check?status=follow">フォロー中</a>'
+      expect(response.body).to_not include '<a class="nav-link" href="/homes/check?status=follower">フォロワー</a>'
+      expect(response.body).to_not include '<a class="nav-link" rel="nofollow" data-method="delete" href="/users/sign_out">ログアウト</a>'
     end
   end
 end
@@ -12,7 +23,42 @@ end
 RSpec.describe 'Homes', type: :request do
   let(:user) { create(:user) }
 
-  describe 'showページ' do
+  describe 'ログインしてindexページを取得する' do
+    before do
+      user.confirm
+      sign_in user
+    end
+    it '取得に成功し、正しいリンクのみ存在する' do
+      get root_path
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include '<a class="navbar-brand" href="/">idimo</a>'
+      expect(response.body).to include '<a class="nav-link" href="/homes/show">ユーザ検索</a>'
+      expect(response.body).to include '<a class="nav-link" href="/users/show?id=' + user.id.to_s + '">マイプロフィール</a>'
+      expect(response.body).to include '<a class="nav-link" href="/users/edit">アカウント編集</a>'
+      expect(response.body).to include '<a class="nav-link" href="/homes/check?status=follow">フォロー中</a>'
+      expect(response.body).to include '<a class="nav-link" href="/homes/check?status=follower">フォロワー</a>'
+      expect(response.body).to include '<a class="nav-link" rel="nofollow" data-method="delete" href="/users/sign_out">ログアウト</a>'
+      expect(response.body).to_not include '<a class="nav-link" href="/users/sign_up">新規登録</a>'
+      expect(response.body).to_not include '<a class="nav-link" href="/users/sign_in">ログイン</a>'
+      expect(response.body).to_not include '<a class="nav-link" rel="nofollow" data-method="post" href="/users/guest_sign_in">ゲストログイン（閲覧用）</a>'
+    end
+  end
+end
+
+RSpec.describe 'Homes', type: :request do
+
+  describe 'ログインせずにshowページを取得する' do
+    it '表示に成功する' do
+      get homes_show_path
+      expect(response).to_not have_http_status(:success)
+    end
+  end
+end
+
+RSpec.describe 'Homes', type: :request do
+  let(:user) { create(:user) }
+
+  describe 'ログインしてshowページを取得する' do
     before do
       user.confirm
       sign_in user
