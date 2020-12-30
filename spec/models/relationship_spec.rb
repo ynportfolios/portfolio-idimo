6c5ab6f91c76152d7e982a8d78df0f53d6dd8cc1
ynportfolios <y.n.portfolios@gmail.com>
2020-12-30 12:00:07 +0000

    テストコードを実装

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-# RSpec.describe Relationship, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Relationship, type: :model do
+  it 'ユーザID、フォローIDがある場合、有効である' do
+    user = FactoryBot.create(:user)
+    user_a = FactoryBot.create(:user_a)
+    relationship = Relationship.new(
+      user_id: user.id,
+      follow_id: user_a.id
+    )
+    expect(relationship).to be_valid
+  end
+  it 'フォローIDがない場合、無効である' do
+    user = FactoryBot.create(:user)
+    relationship = Relationship.new(
+      user_id: user.id,
+      follow_id: nil
+    )
+    relationship.valid?
+    expect(relationship.errors[:follow_id]).to include('を入力してください')
+  end
+  it 'ユーザIDがない場合、無効である' do
+    user_a = FactoryBot.create(:user_a)
+    relationship = Relationship.new(
+      user_id: nil,
+      follow_id: user_a.id
+    )
+    relationship.valid?
+    expect(relationship.errors[:user_id]).to include('を入力してください')
+  end
+end
