91daaf38d522f7b6a3b14d0d5f89c6028002e4a3
ynportfolios <y.n.portfolios@gmail.com>
2020-12-31 19:44:03 +0900

    users_controller.rbのテストコードを実装

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,12 +1,40 @@
 require 'rails_helper'
 
-# RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
+  let(:user) { create(:user) }
+  let(:user_a) { create(:user_a) }
 
-#   describe "GET /show" do
-#     it "returns http success" do
-#       get "/users/show"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-# end
+  describe 'ログインしてshowページを取得する' do
+    before do
+      user.confirm
+      user_a.confirm
+      sign_in user
+    end
+    it 'ユーザが自身のshowページの取得に成功する' do
+      get users_show_path(id: user.id)
+      expect(response).to have_http_status(:success)
+    end
+    it 'ユーザが他者のshowページの取得に成功する' do
+      # ダイレクトメッセージ機能で使用するroomとentryをインスタンス化する
+      get users_show_path(id: user_a.id)
+      expect(response).to have_http_status(:success)
+    end
+    it 'ユーザが他者のshowページの取得に成功する' do
+      # ダイレクトメッセージ機能で使用するroomを取得する
+      room = Room.create
+      expect(room).to be_valid
+      entry = Entry.create(
+        user_id: user.id,
+        room_id: room.id
+      )
+      expect(entry).to be_valid
+      entry_a = Entry.create(
+        user_id: user_a.id,
+        room_id: room.id
+      )
+      expect(entry_a).to be_valid
+      get users_show_path(id: user_a.id)
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
