873680250f1b5b6d14837800c8958d0bc7a20c49
ynportfolios <y.n.portfolios@gmail.com>
2021-01-24 17:54:02 +0900

    メイラーの設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # 20201106 CICDの構築 start
-  config.action_mailer.default_url_options = { host: 'portfolio-idimo.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'qiita-ecs-lb-1989771647.ap-northeast-1.elb.amazonaws.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
