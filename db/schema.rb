ab30f6e71cd247073bf70a252ceb5e36e9a7f718
ynportfolios <y.n.portfolios@gmail.com>
2021-02-05 13:32:20 +0900

    スキーマの修正（Relationships）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_040853) do
+ActiveRecord::Schema.define(version: 2021_02_05_041808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 2021_02_05_040853) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "follow_id"
+    t.bigint "user_id", null: false
+    t.bigint "follow_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["follow_id"], name: "index_relationships_on_follow_id"
