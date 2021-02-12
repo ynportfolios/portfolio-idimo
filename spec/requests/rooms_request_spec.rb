b7313195ea776c65d0b110e08b5cff474ec633d2
ynportfolios <y.n.portfolios@gmail.com>
2021-02-12 11:16:20 +0900

    ダイレクトメッセージ一覧ページの作成

--- spec/requests/rooms_request_spec.rb
+++ spec/requests/rooms_request_spec.rb
@@ -50,3 +50,35 @@ RSpec.describe 'Rooms', type: :request do
     end
   end
 end
+
+RSpec.describe 'Rooms', type: :request do
+  let(:user) { create(:user) }
+  let(:user_a) { create(:user_a) }
+
+  describe 'ログインしてindexページを取得する' do
+    before do
+      user.confirm
+      sign_in user
+    end
+    it '取得に成功する（Entryが存在する場合）' do
+      room = Room.create
+      expect(room).to be_valid
+      entry = Entry.create(
+        user_id: user.id,
+        room_id: room.id
+      )
+      expect(entry).to be_valid
+      entry_a = Entry.create(
+        user_id: user_a.id,
+        room_id: room.id
+      )
+      expect(entry_a).to be_valid
+      get rooms_path
+      expect(response).to have_http_status(:success)
+    end
+    it '取得に成功する（Entryが存在しない場合）' do
+      get rooms_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
