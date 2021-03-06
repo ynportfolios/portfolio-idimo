eb39854dd975cfe4f0c0ac391cbf55d743b51f57
ynportfolios <y.n.portfolios@gmail.com>
2021-03-06 16:12:42 +0000

    トップページの編集

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,25 +1,5 @@
 class HomesController < ApplicationController
-  def index
-    @users = User.all
-    @man_count = 0
-    @woman_count = 0
-    @other_count = 0
-    @no_answer_count = 0
-    @users.each do |user|
-      case user.sex
-      when 'man'
-        @man_count += 1
-      when 'woman'
-        @woman_count += 1
-      when 'other'
-        @other_count += 1
-      when 'no_answer'
-        @no_answer_count += 1
-      else
-        @no_answer_count += 1
-      end
-    end
-  end
+  def index; end
 
   def show
     @q = User.ransack(params[:q])
